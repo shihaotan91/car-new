51ad4ceab16908194ed7996e9636486951e184f7
fauzi mohd <M.fauzi_87@hotmail.com>
2016-10-10 14:38:20 +0800

    added decelerate and stop

---
+++ app.js
@@ -0,0 +1,6 @@
+// var CarObj = require('./Car')
+//
+// var new_car = new CarObj('blue', 'true')
+//
+// console.log(new_car.decelerate(2) + ' km/h')
+// console.log(new_car.stop() + ' km/h')
--- car.js
+++ car.js
@@ -3,6 +3,12 @@ var Car = function(color, convertible){
   this.speed = 0
   this.convertible = convertible
 }
+Car.prototype.decelerate = function(num){
+  this.speed = this.speed + num
+}
+
+Car.prototype.stop = function(){
+   this.speed = 0
+}
 
 module.exports = Car
- //test test
